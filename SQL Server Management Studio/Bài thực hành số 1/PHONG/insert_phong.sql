327df1bd3d18db7d25e32a3876ad03dfcd62ec96
Th@ng12350 <72205096+QuangThang2197@users.noreply.github.com>
2022-02-20 19:12:00 +0700

    Add files via upload

---
+++ SQL Server Management Studio/Bài thực hành số 1/PHONG/insert_phong.sql
@@ -0,0 +1,12 @@
+﻿use QLNV
+go
+
+insert into PHONG (MAPHONG, TENPHONG, DIACHI, TEL) 
+values ('HCA', N'Hành chính tổ hợp', N'123, Láng Hạ, Đống Đa, Hà Nội', '04 8585793')
+insert into PHONG (MAPHONG, TENPHONG, DIACHI, TEL) 
+values ('KDA', N'Kinh Doanh', N'123, Láng Hạ, Đống Đa, Hà Nội', '04 8574943')
+insert into PHONG (MAPHONG, TENPHONG, DIACHI, TEL) 
+values ('KTA', N'Kỹ thuật', N'123, Láng Hạ, Đống Đa, Hà Nội', '04 9480485')
+insert into PHONG (MAPHONG, TENPHONG, DIACHI, TEL) 
+values ('QTA', N'Quản trị', N'123, Láng Hạ, Đống Đa, Hà Nội', '04 8508585')
+go
---
+++ SQL Server Management Studio/Bài thực hành số 1/TDNN/insert_tdnn.sql
@@ -0,0 +1,31 @@
+use QLNV
+go
+
+insert into TDNN (MANV, MANN, TDO) VALUES ('HC001','01', 'A')
+insert into TDNN (MANV, MANN, TDO) VALUES ('HC001','02', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('HC002','01', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('HC002','03', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('HC003','01', 'D')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD001','01', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD001','02', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD002','01', 'D')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD002','02', 'A')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD003','01', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD003','02', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD004','01', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD004','04', 'A')
+
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD004','05', 'A')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD005','01', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD005','02', 'D')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD005','03', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KD005','04', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT001','01', 'D')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT001','04', 'E')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT002','01', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT002','02', 'B')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT003','01', 'D')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT003','03', 'C')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT004','01', 'D')
+insert into TDNN (MANV, MANN, TDO) VALUES ('KT005','01', 'C')
+go

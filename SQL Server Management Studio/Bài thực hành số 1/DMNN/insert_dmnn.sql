3a1312e508c1bcdaf90e2f9e7d923fc2ed90e1fe
Th@ng12350 <72205096+QuangThang2197@users.noreply.github.com>
2022-02-25 07:33:48 +0700

    Update insert_dmnn.sql

--- SQL Server Management Studio/Bài thực hành số 1/DMNN/insert_dmnn.sql
+++ SQL Server Management Studio/Bài thực hành số 1/DMNN/insert_dmnn.sql
@@ -1,8 +1,12 @@
-﻿insert into DMNN (MANN, TENNN) values ('01', N'Anh')
+USE QLNV
+GO
+
+insert into DMNN (MANN, TENNN) values ('01', N'Anh')
 insert into DMNN (MANN, TENNN) values ('02', N'Nga')
 insert into DMNN (MANN, TENNN) values ('03', N'Pháp')
 insert into DMNN (MANN, TENNN) values ('04', N'Nhật')
 insert into DMNN (MANN, TENNN) values ('05', N'Trung Quốc')
 insert into DMNN (MANN, TENNN) values ('06', N'Hàn Quốc')
+GO
 
 
